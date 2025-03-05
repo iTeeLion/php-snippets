b9db0ceeaa3280ee5b38fbd5fc2f2512cc161d49
Sergey Abu <dev@iteelion.ru>
2025-03-05 14:29:34 +0300

    Update dump2file.php

--- dump2file.php
+++ dump2file.php
@@ -2,4 +2,5 @@
 
 ob_start();
 var_dump($var);
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/_debug', ob_end_clean(), FILE_APPEND);
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/_debug.txt', ob_get_contents(), FILE_APPEND);
+ob_end_clean();
